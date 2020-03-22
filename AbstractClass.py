7708d770625b54d8d2f0b2a7207b16faadddf756
Toufiqul-Islam-Noyon <45868123+Toufiqul-Islam-Noyon@users.noreply.github.com>
2020-03-23 00:14:02 +0600

    Add files via upload

---
+++ AbstractClass.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+class Bike(ABC):
+    __Bike_Name=None
+    __Bike_Price=None
+    @abstractmethod
+    def display(self): pass
+    @abstractmethod
+    def calculate_Milage(self): pass
+
+class Honda(Bike):
+    def display(self):
+        print('Honda is a good bike')
+
+    def calculate_Milage(self):
+        print('Honda gives 40 km/l')
+
+Honda_Obj= Honda()
+Honda_Obj.display()
+Honda_Obj.calculate_Milage()
